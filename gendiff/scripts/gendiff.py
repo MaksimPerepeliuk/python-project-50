87c9e1c4356f5deb4e5a7840734d2e295519a601
MaksimPerepeliuk <max_perepelyuk@mail.ru>
2024-08-14 15:11:59 +0300

    add -f set input/outpu format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,9 +4,10 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',
-        usage='gendiff [-h] first_file second_file'
+        usage='gendiff [-h] [-f FORMAT] first_file second_file'
     )
 
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('first_file', help='First configuration file')
     parser.add_argument('second_file', help='Second configuration file')
 
