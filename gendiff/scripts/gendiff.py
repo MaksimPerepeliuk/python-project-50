7b69815b901920087e42a50c2f9a117b77593572
MaksimPerepeliuk <max_perepelyuk@mail.ru>
2024-08-14 11:22:21 +0300

    create cli input files

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.',
+        usage='gendiff [-h] first_file second_file'
+    )
+
+    parser.add_argument('first_file', help='First configuration file')
+    parser.add_argument('second_file', help='Second configuration file')
+
+    args = parser.parse_args()
+
+    print(f'Comparing {args.first_file} and {args.second_file}')
+
+
+if __name__ == '__main__':
+    main()
