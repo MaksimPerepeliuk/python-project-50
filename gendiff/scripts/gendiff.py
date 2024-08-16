9fabc907a9195be8fbefc202139da9c5c7c212da
MaksimPerepeliuk <max_perepelyuk@mail.ru>
2024-08-16 16:26:34 +0300

    create gendiff between two json files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,8 @@
 import argparse
+import json
 
 
-def main():
+def get_filepaths_cli():
     parser = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.',
         usage='gendiff [-h] [-f FORMAT] first_file second_file'
@@ -13,7 +14,41 @@ def main():
 
     args = parser.parse_args()
 
-    print(f'Comparing {args.first_file} and {args.second_file}')
+    first_file_path, second_file_path = args.first_file, args.second_file
+
+    print(f'Comparing {first_file_path} and {second_file_path}')
+
+    return first_file_path, second_file_path
+
+
+def generate_diff(obj1, obj2):
+    items1 = obj1.items()
+    items2 = obj2.items()
+
+    diff_actions = {
+        '  ': lambda items1, items2: set(items1) & set(items2),
+        '- ': lambda items1, items2: set(items1) - set(items2),
+        '+ ': lambda items1, items2: set(items2) - set(items1)
+    }
+
+    diff_items = []
+
+    for diff_char, diff_func in diff_actions.items():
+        diff_items = [*diff_items,
+                      *[(diff_char, k, v)
+                        for k, v in diff_func(items1, items2)]]
+
+    diff_dict = {f'{c}{k}': v
+                 for c, k, v in sorted(diff_items, key=lambda item: item[1])}
+
+    return json.dumps(diff_dict, indent=2)
+
+
+def main():
+    first_file_path, second_file_path = get_filepaths_cli()
+    first_file = json.load(open(first_file_path))
+    second_file = json.load(open(second_file_path))
+    print(generate_diff(first_file, second_file))
 
 
 if __name__ == '__main__':
